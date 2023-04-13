ce3003461a7199f2e99ce91a0d95ce5727b37c91
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-13 11:09:28 -0600

    add types

--- src/python/sigopt_config/broker.py
+++ src/python/sigopt_config/broker.py
@@ -3,43 +3,54 @@
 # SPDX-License-Identifier: Apache License 2.0
 import functools
 import os
+from typing import Any, Iterable
 
 import jmespath
 import json_merge_patch
 import yaml
 
 
+__all__ = ["ConfigBroker"]
+
+
 class ConfigBroker(object):
+  data: dict
+
   @classmethod
-  def from_configs(cls, configs):
+  def from_configs(cls, configs: Iterable[dict]) -> "ConfigBroker":
     merged_config = functools.reduce(json_merge_patch.merge, reversed(list(configs)), {})
     return cls(merged_config)
 
   @classmethod
-  def from_directory(cls, dirname):
+  def from_directory(cls, dirname: str) -> "ConfigBroker":
     configs = []
     for config_path in sorted(os.listdir(dirname)):
       with open(os.path.join(dirname, config_path)) as config_fp:
-        configs.append(yaml.safe_load(config_fp))
+        data = yaml.safe_load(config_fp)
+        if data is None:
+          continue
+        if not isinstance(data, dict):
+          raise TypeError(f"Top level of config is not a mapping: {config_fp.name}")
+        configs.append(data)
     return cls.from_configs(configs)
 
-  def __init__(self, data):
+  def __init__(self, data: dict):
     self.data = data
 
-  def __contains__(self, name):
+  def __contains__(self, name: str) -> bool:
     try:
       self[name]  # pylint: disable=pointless-statement
       return True
     except KeyError:
       return False
 
-  def get(self, name, default=None):
+  def get(self, name: str, default: Any = None) -> Any:
     try:
       return self[name]
     except KeyError:
       return default
 
-  def __getitem__(self, name):
+  def __getitem__(self, name: str) -> Any:
     value = jmespath.search(name, self.data)
     if value is None:
       raise KeyError(name)
